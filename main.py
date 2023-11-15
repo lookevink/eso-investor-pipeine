5ce1c1956cb147dc0f7f0dcdc9dfad9f399995de
johnpatrickroach <johnpatrickroach1@gmail.com>
2023-11-15 17:33:23 -0500

    Progress on CA.

--- main.py
+++ main.py
@@ -113,3 +113,99 @@ async def get_entity_data(entities: EntityNames) -> object:
     return StreamingResponse(csv_output, media_type="text/csv", headers={
         "Content-Disposition": "attachment; filename=output.csv"
     })
+
+
+@app.post("/ca/get_member_data/")
+async def get_entity_data(entities: EntityNames) -> object:
+    """
+    Retrieves entity data from the California Secretary of State website for the given list of entity names.
+
+    Args:
+        entities (EntityNames): The list of entity names.
+
+    Returns:
+        object: A StreamingResponse object containing the entity data.
+
+    """
+    logging.info("Starting the get_entity_data function")
+    csv_output = StringIO()
+    csv_writer = csv.DictWriter(csv_output, fieldnames=['Title', 'Name', 'Address', 'BusinessName'])
+    csv_writer.writeheader()
+
+    for entity_name in entities.names:
+        logging.info(f"Processing entity: {entity_name}")
+        time.sleep(5)  # Rate limit wait
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/119.0',
+            'Accept': '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Prefer': 'safe',
+            'Referer': 'https://bizfileonline.sos.ca.gov/search/business',
+            'authorization': 'undefined',
+            'content-type': 'application/json',
+            'Origin': 'https://bizfileonline.sos.ca.gov',
+            'DNT': '1',
+            'Connection': 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-origin',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+        }
+        json_data = {
+            'SEARCH_VALUE': f'{entity_name}',
+            'SEARCH_FILTER_TYPE_ID': '0',
+            'SEARCH_TYPE_ID': '1',
+            'FILING_TYPE_ID': '',
+            'STATUS_ID': '',
+            'FILING_DATE': {
+                'start': None,
+                'end': None,
+            },
+            'CORPORATION_BANKRUPTCY_YN': False,
+            'CORPORATION_LEGAL_PROCEEDINGS_YN': False,
+            'OFFICER_OBJECT': {
+                'FIRST_NAME': '',
+                'MIDDLE_NAME': '',
+                'LAST_NAME': '',
+            },
+            'NUMBER_OF_FEMALE_DIRECTORS': '99',
+            'NUMBER_OF_UNDERREPRESENTED_DIRECTORS': '99',
+            'COMPENSATION_FROM': '',
+            'COMPENSATION_TO': '',
+            'SHARES_YN': False,
+            'OPTIONS_YN': False,
+            'BANKRUPTCY_YN': False,
+            'FRAUD_YN': False,
+            'LOANS_YN': False,
+            'AUDITOR_NAME': '',
+        }
+        response = requests.post(
+            'https://bizfileonline.sos.ca.gov/api/Records/businesssearch',
+            headers=headers,
+            json=json_data,
+        )
+
+        if response.status_code != 200:
+            logging.warning(f"Entity {entity_name} returned {response.status_code}. Skipping.")
+            continue
+
+        entities = response.json().get('rows', '')
+        logging.info(f"Found {len(entities)} entity numbers for entity {entity_name}")
+
+        for entity in entities:
+            time.sleep(5)  # Rate limit wait
+            response = requests.get(
+                f'https://bizfileonline.sos.ca.gov/api/FilingDetail/business/{entity}/false',
+                headers=headers,
+            )
+            for obj in response.json()['DRAWER_DETAIL_LIST']:
+                if obj['LABEL'] == "Agent":
+                    business_name = obj['VALUE']
+                    print(f"Processing business {business_name}")
+
+
+@app.get("/")
+def read_root():
+    return {"Title": "Investor Database Project"}
